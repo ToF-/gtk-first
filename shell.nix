d83f3cc2fcc6c690fff059c92121e2c3ae951b39
FaustXVI <xavier.detant@gmail.com>
2022-09-25 21:43:13 +0200

    Add nix config

---
+++ shell.nix
@@ -0,0 +1,19 @@
+let
+  # Last updated: 2/26/21. Update as necessary from https://status.nixos.org/...
+  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/22.05.tar.gz")) {};
+
+  # Rolling updates, not deterministic.
+  # pkgs = import (fetchTarball("channel:nixpkgs-unstable")) {};
+in pkgs.mkShell {
+  buildInputs = [
+    pkgs.cargo
+    pkgs.rustc
+    pkgs.rustfmt
+    pkgs.glib
+    pkgs.pkg-config
+    pkgs.gtk3
+  ];
+
+  # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela.
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+}
