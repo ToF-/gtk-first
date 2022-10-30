c51d4b1fb9979a0e208e9468210dd8fdeb03f372
FaustXVI <xavier.detant@gmail.com>
2022-10-30 21:41:58 +0100

    Macro for testing points

--- src/lines.rs
+++ src/lines.rs
@@ -42,6 +42,86 @@ pub fn create_lines(points: NumberOfPoints, factor: JunctionFactor) -> Vec<Line>
 mod tests {
     use super::*;
 
+    mod assertions {
+        use super::*;
+        macro_rules! assert_eq_delta {
+            ($x:expr, $y:expr, $delta:expr) => {
+                if(f64::abs($x - $y) > $delta) {panic!();}
+            };
+        }
+
+        macro_rules! assert_points_eq {
+            ($x:expr, $y:expr) => {
+                assert_eq_delta!($x.abscissa, $y.abscissa,0.0001);
+                assert_eq_delta!($x.ordinate, $y.ordinate,0.0001);
+            };
+        }
+
+        #[test]
+        fn allows_for_delta_in_comparison() {
+            assert_eq_delta!(2.0, 2.0+0.01, 0.1);
+        }
+
+        #[test]
+        fn allows_for_delta_in_comparison_for_negatives() {
+            assert_eq_delta!(-2.0, -2.0+0.01, 0.1);
+        }
+
+        #[test]
+        #[should_panic]
+        fn panic_when_delta_passed() {
+            assert_eq_delta!(2.0+0.000011, 2.0, 0.00001)
+        }
+
+        #[test]
+        #[should_panic]
+        fn panic_when_delta_passed_by_absolute_difference() {
+            assert_eq_delta!(2.0, 2.0+0.000011, 0.00001)
+        }
+
+        #[test]
+        fn two_points_are_equals_with_a_difference_under_1_over_1000() {
+            let a = Point {
+                abscissa: 0f64,
+                ordinate: 1f64,
+            };
+            let b = Point {
+                abscissa: 0f64 + 0.00001f64,
+                ordinate: 1f64 + 0.00001f64,
+            };
+            assert_points_eq!(a,b);
+            assert_points_eq!(b,a);
+        }
+
+        #[test]
+        #[should_panic]
+        fn two_points_are_not_equals_with_a_difference_over_1_over_1000() {
+            let a = Point {
+                abscissa: 0f64,
+                ordinate: 1f64,
+            };
+            let b = Point {
+                abscissa: 0f64 + 0.001f64,
+                ordinate: 1f64 + 0.001f64,
+            };
+            assert_points_eq!(a,b);
+        }
+
+        #[test]
+        #[should_panic]
+        fn two_points_are_not_equals_with_a_difference_over_1_over_1000_both_ways() {
+            let a = Point {
+                abscissa: 0f64,
+                ordinate: 1f64,
+            };
+            let b = Point {
+                abscissa: 0f64 + 0.001f64,
+                ordinate: 1f64 + 0.001f64,
+            };
+            assert_points_eq!(b,a);
+        }
+    }
+
     #[test]
     fn creates_2_lines_for_2_points() {
         let lines = create_lines(NumberOfPoints(2), JunctionFactor(1f64));
